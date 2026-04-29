2ac8d41c17a7b0ab688f6502d930c2bd4f0ff347
Amira Abdel-Fattah <amiraelmezien@gmail.com>
2026-04-29 13:57:51 +0300

    Format date time
    Signed-off-by: AmiraAbdel-fatah <amiraelmezien@gmail.com>

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes/models/note_model.dart';
 
@@ -7,18 +8,17 @@ import 'custom_button.dart';
 import 'custom_text_field.dart';
 
 class AddNoteForm extends StatefulWidget {
-  AddNoteForm({Key? key}) : super(key: key);
+  const AddNoteForm({super.key});
 
   @override
   State<AddNoteForm> createState() => _AddNoteFormState();
 }
 
-final GlobalKey<FormState> formKey = GlobalKey();
-AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
-String? title;
-String? subTitle;
-
 class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -26,14 +26,14 @@ class _AddNoteFormState extends State<AddNoteForm> {
       autovalidateMode: autoValidateMode,
       child: Column(
         children: [
-          SizedBox(height: 32),
+          const SizedBox(height: 32),
           CustomTextField(
             hint: 'Title',
             onSaved: (value) {
               title = value;
             },
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           CustomTextField(
             hint: 'Content',
             maxLines: 5,
@@ -41,16 +41,19 @@ class _AddNoteFormState extends State<AddNoteForm> {
               subTitle = value;
             },
           ),
-          SizedBox(height: 50),
+          const SizedBox(height: 50),
           CustomButton(
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
-
-                var noteModel = NoteModel(title: title!,
-                    subtitle: subTitle!,
-                    date: DateTime.now().toString(),
-                    color: Colors.blue.value);
+                var currentDate = DateTime.now();
+                var formattedCurrentDate = DateFormat.yMd().format(currentDate);
+                var noteModel = NoteModel(
+                  title: title!,
+                  subtitle: subTitle!,
+                  date: formattedCurrentDate,
+                  color: Colors.blue.value,
+                );
                 BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               } else {
                 autoValidateMode = AutovalidateMode.always;
@@ -58,7 +61,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
               }
             },
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
         ],
       ),
     );
